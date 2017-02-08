59f5870e637d7c93501d0ec3a800fcb02f7becd6
lcojunk <lcojunk@gmail.com>
2017-02-08 16:33:57 +0100

    added more details in comment

--- src/main/java/leo/example/ChecktestApplication.java
+++ src/main/java/leo/example/ChecktestApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * Simple check Spring programm
  *
  * @author odzhara-ongom
  */
