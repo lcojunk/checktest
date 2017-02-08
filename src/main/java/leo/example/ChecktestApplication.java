6814fe23c024f4ad7b0137f8df5b56a3750acd32
lcojunk <lcojunk@gmail.com>
2017-02-08 15:45:44 +0100

    added autor in main class

--- src/main/java/leo/example/ChecktestApplication.java
+++ src/main/java/leo/example/ChecktestApplication.java
@@ -3,10 +3,14 @@ package leo.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author odzhara-ongom
+ */
 @SpringBootApplication
 public class ChecktestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChecktestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChecktestApplication.class, args);
+    }
 }
